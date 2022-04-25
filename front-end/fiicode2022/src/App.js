0288208a1c15ac092f703920858b20bfd2d9dbfa
CatalinButacu <ionel-catalin.butacu@student.tuiasi.ro>
2022-04-25 10:44:55 +0300

    update react components

--- front-end/fiicode2022/src/App.js
+++ front-end/fiicode2022/src/App.js
@@ -6,17 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <i>App in construction...</i>    
+        <br/>
+        <p>Proba WebDev | FiiCode 2022</p>
+        <p>Echipa: Paul&Catalin</p>
+     
       </header>
     </div>
   );
