8afddfe62a13fe952c4acf00a07d19cc2dd1cad2
CatalinButacu <ionel-catalin.butacu@student.tuiasi.ro>
2022-04-25 11:36:34 +0300

    update react dom

--- front-end/fiicode2022/src/App.js
+++ front-end/fiicode2022/src/App.js
@@ -1,20 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <i>App in construction...</i>    
-        <br/>
+        <img src={logo} className="App-logo" alt="logo" />        
+        <i>App in construction...</i><br/>
         <p>Proba WebDev | FiiCode 2022</p>
-        <p>Echipa: Paul&Catalin</p>
-     
+        <p>Echipa: Paul&Catalin</p>     
       </header>
     </div>
   );
 }
 
+function section(){
+  return(
+    <div>
+      <p>Sectiune noua</p>
+    </div>
+  )
+}
+
+
 export default App;
+
